6d695f7ab72155d1be02543a674935fb95411dc8
Joao Norim <norim_13@hotmail.com>
2015-11-30 12:16:54 +0000

    BaseEquipmentBehaviour falta apenas tempo do tratamento. protocolo CFP completo. NÃO testado.

--- app/src/hospital/agents/behaviours/BaseEquipmentBehaviour.java
+++ app/src/hospital/agents/behaviours/BaseEquipmentBehaviour.java
@@ -1,5 +1,6 @@
 package hospital.agents.behaviours;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import hospital.agents.EquipmentAgent;
@@ -13,17 +14,23 @@ public class BaseEquipmentBehaviour extends Behaviour {
 	/**
 	 * States: 
 	 * 0 -> waiting for patient
-	 * 1 -> receiving proposals
-	 * 2 -> waiting for patient confirmation
-	 * 3 -> being used by patient 
+	 * 1 -> cfp sent. receiving proposals
+	 * 2 -> all proposals received. sending answers.
+	 * 3 -> answers sent. waiting for patient confirmation
+	 * 4 -> being used by patient 
 	 */
 	private int state = 0;
 	
 	private String replyWith = null;
 	private MessageTemplate mt; // The template to receive replies
 	private int proposalsSent; //number of Patients that CFP was sent to
-	private int proposalsReceived;
+
+	private List<AID> proposalsReceivedAIDs = new ArrayList<AID>();
+	private AID bestProposalAID = null;
 	private int bestProposal = 0;
+	
+	private boolean acceptedByPatient = false;
+	private int finalAnswersReceived = 0;
 	
 	public BaseEquipmentBehaviour(EquipmentAgent equipmentAgent) {
 		myAgent = equipmentAgent;
@@ -40,27 +47,86 @@ public class BaseEquipmentBehaviour extends Behaviour {
 		case 0: 
 			initiateContractNet();
 			bestProposal = 0;
+			bestProposalAID = null;
+			proposalsReceivedAIDs = new ArrayList<AID>();
 			this.state++;
 			break;
 		case 1:
 			receiveProposal();
-			if(this.proposalsReceived == this.proposalsSent)
+			if(this.proposalsReceivedAIDs.size() == this.proposalsSent)
 				this.state++;
 			break;
 		case 2:
-			
+			acceptedByPatient = false;
+			finalAnswersReceived = 0;
+			sendProposalResponses();
 			break;
 		case 3:
-			
+			receiveFinalResponses();
+			if(finalAnswersReceived == proposalsSent){
+				if(acceptedByPatient)
+					this.state++;
+				else this.state = 0;
+			}			
+			break;		
+		case 4:
+			// wait for patient to check in??
+			// + 
+			// wait for treatment to finish.
 			break;
 		}
 		
 		
 	}
 
-	private void receiveProposal() {
-		// TODO Auto-generated method stub
+	private void receiveFinalResponses() {
+		ACLMessage msg = myAgent.receive(mt);
+		if (msg != null) {
+			if(msg.getSender() == this.bestProposalAID)
+				if(msg.getPerformative() == ACLMessage.INFORM){
+					this.acceptedByPatient = true;
+				}
+			this.finalAnswersReceived++;
+		}
+		else {
+			block();
+		}		
+	}
+
+	private void sendProposalResponses() {
+		// REJECT
+		ACLMessage reject = new ACLMessage(ACLMessage.REJECT_PROPOSAL);
 		
+		for (int j = 0; j < this.proposalsReceivedAIDs.size(); j++) {
+			if(this.proposalsReceivedAIDs.get(j) != this.bestProposalAID)
+				reject.addReceiver(this.proposalsReceivedAIDs.get(j));
+		}
+		reject.setConversationId("auction-equipment-usage");
+		reject.setReplyWith(replyWith); // Unique value
+		myAgent.send(reject);
+		
+		// ACCEPT 
+		ACLMessage accept = new ACLMessage(ACLMessage.ACCEPT_PROPOSAL);
+		accept.addReceiver(this.bestProposalAID);
+		accept.setConversationId("auction-equipment-usage");
+		reject.setReplyWith(replyWith); // Unique value
+		myAgent.send(accept);		
+	}
+
+	private void receiveProposal() {
+		ACLMessage msg = myAgent.receive(mt);
+		if (msg != null) {
+			
+			int proposal = Integer.parseInt(msg.getContent());
+			if(proposal > this.bestProposal){
+				this.bestProposal = proposal;
+				this.bestProposalAID = msg.getSender();
+			}
+			this.proposalsReceivedAIDs.add(msg.getSender());
+		}
+		else {
+			block();
+		}		
 	}
 
 	private void initiateContractNet() {
